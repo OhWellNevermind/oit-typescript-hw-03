bd88764e43fd60f8cbd2382cc8c36fe6004f7fcd
OhWellNevermind <52574266+OhWellNevermind@users.noreply.github.com>
2023-10-25 23:59:41 +0300

    hw done

--- src/4.ts
+++ src/4.ts
@@ -1,11 +1,61 @@
+class Key {
+  private signature: number;
+  constructor() {
+    this.signature = Math.random();
+  }
+
+  getSignature() {
+    return this.signature;
+  }
+}
+
+class Person {
+  private key: Key;
+  constructor(key: Key) {
+    this.key = key;
+  }
+
+  getKey() {
+    return this.key;
+  }
+}
+
+abstract class House {
+  door: boolean;
+  key: Key;
+  tenants: Array<Person> = [];
+
+  comeIn(person: Person) {
+    if (this.door) {
+      console.log("You welcome in!");
+      this.tenants.push(person);
+    }
+  }
+
+  openDoor(key: Key) {}
+}
+
+class MyHouse extends House {
+  constructor(key: Key) {
+    super();
+    this.key = key;
+  }
+
+  openDoor(key: Key) {
+    if (key.getSignature() === this.key.getSignature()) {
+      console.log("Door is open");
+      this.door = true;
+    }
+  }
+}
+
 const key = new Key();
 
-const house = new MyHouse(key);
 const person = new Person(key);
+const house = new MyHouse(key);
 
 house.openDoor(person.getKey());
 
 house.comeIn(person);
-
 
 export {};
